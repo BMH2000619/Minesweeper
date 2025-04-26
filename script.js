7b3167a1c41d39d0a1cbdd9808b470d84cd6f715
BMH2000619 <baselmohammedb.m@gmail.com>
2025-04-26 21:49:41 +0300

    Added Functions, calc,flag,show,handle

--- script.js
+++ script.js
@@ -38,12 +38,76 @@ const boardCreate = () => {
     })
 
     // https://stackoverflow.com/questions/28790209/attaching-an-event-listener-for-left-or-right-click-onclick-doesnt-work-for-r
-    cell.addEventListener('contextmenu', () => {
+    cell.addEventListener('contextmenu', (e) => {
       // https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
-      preventDefault()
+      e.preventDefault()
+      toggleFlag(cell)
     })
 
     main.appendChild(cell)
     cells.push(cell)
   })
+  calcNumbers()
 }
+
+const calcNumbers = () => {
+  for (let i = 0; i < cells.length; i++) {
+    const isLeftEdge = i % width === 0
+    const isRightEdge = i % width === width - 1
+
+    let total = 0
+
+    if (cells[i].dataset.mine === 'true') continue
+
+    const adjacent = []
+    if (!isLeftEdge && i > 0) adjacent.push(i - 1)
+    if (!isRightEdge && i < 99) adjacent.push(i + 1)
+    adjacent.push(i - width)
+    adjacent.push(i + width)
+    if (!isLeftEdge && i - width > 0) adjacent.push(i - width - 1)
+    if (!isRightEdge && i - width >= 0) adjacent.push(i - width + 1)
+    if (!isLeftEdge && i + width < 100) adjacent.push(i + width - 1)
+    if (!isRightEdge && i + width < 100) adjacent.push(i + width - 1)
+
+    const validAdjacent = adjacent.filter(
+      (n) => cells[n].dataset.mine === 'true'
+    )
+
+    cells[i].dataset.number = validAdjacent.length
+  }
+}
+
+let handleClick = (cell) => {
+  if (cell.classList.contains('flag')) {
+    return
+  }
+
+  if (cell.dataset.mine === 'true') {
+    cell.classList.add('mine')
+    alert('Game Over!')
+    revealAllMines()
+    return
+  }
+}
+
+// Flags when you right click on a cell
+const toggleFlag = (cell) => {
+  if (cell.classList.contains('safe-cell')) {
+    return
+  }
+  //https://www.w3schools.com/jquery/eff_toggle.asp#:~:text=The%20toggle()%20method%20toggles,This%20creates%20a%20toggle%20effect.
+  cell.classList.toggle('flag')
+}
+
+// Shows all the mines when a mine is clicked
+const showAllMines = () => {
+  cells.forEach((cell) => {
+    if (cell.dataset.mine === 'true') {
+      cell.classList.add('mine')
+    }
+  })
+}
+
+reset.addEventListener('click', boardCreate())
+
+boardCreate()
